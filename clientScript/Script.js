397ce9d452476dfc5be493bc1ea7f8af41c90f2d
selbstbeherrschung <40072614+selbstbeherrschung@users.noreply.github.com>
2020-08-18 15:06:32 +0300

    v1.2
    AJAX added

--- clientScript/Script.js
+++ clientScript/Script.js
@@ -1,27 +1,73 @@
 var checkSetR = false;
+var servDom = 'http://localhost';
 
-function checkText(f){
+
+function checkText() {
     var str = text_input.value;
-    if (isNaN(str)){
+    if (isNaN(str)) {
         alert(str + " is wrong. You should write int number!");
-    }else {
+    } else {
         var i = parseFloat(str);
-        if(i >= -5 && i <=3) {
+        if (i >= -5 && i <= 3) {
             text_input.value = i;
-            if(checkSetR){
-                f.submit();
-            }else {
+            if (checkSetR) {
+                return true;
+            } else {
                 alert("Please, choose R by button");
             }
 
-        }else {
+        } else {
             alert(str + " isn't in range(-5:3)!");
         }
     }
+    return false;
 }
 
 function setR(int) {
-    document.getElementById("vR").setAttribute("value",int);
-    alert("Done");
+    document.getElementById("vR").setAttribute("value", int);
+    var rNames = document.getElementsByName("rChoose");
+    for (let i = 0; i < rNames.length; i++) {
+        rNames[i].setAttribute("class", "inputNe");
+    }
+    rNames[(int*2)-2].setAttribute("class", "inputOk");
     checkSetR = true;
 }
+
+const submit = function (e) {
+    if (!checkText()) return e.preventDefault();
+
+    let xVal;
+    let ansX = document.getElementsByName("answerX");
+    for (let i = 0; i < ansX.length; i++) {
+        if (ansX[i].checked) {
+            xVal = ansX[i].value
+            break;
+        }
+    }
+    var yVal = document.getElementById("text_input").value;
+    var rVal = document.getElementById("vR").value;
+
+    let xhr = new XMLHttpRequest();
+
+    let url = new URL('/web-lab-1/serverScript/handler.php', servDom);
+
+    url.searchParams.append('answerX', xVal);
+    url.searchParams.append('answerY', yVal);
+    url.searchParams.append('answerR', rVal);
+
+    xhr.open('GET', url);
+
+    alert("Open: " + url + " ");
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            document.getElementById("result").innerHTML = xhr.responseText
+        }
+    }
+
+    xhr.send()
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('submitButton').addEventListener('click', submit);
+});
--- serverScript/handler.php
+++ serverScript/handler.php
@@ -1,120 +1,86 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD html 4.01//EN"
-        "http://www.w3.org/TR/html4/strict.dtd">
-<HTML>
-<head>
-    <title>
-        Lab 1
-    </title>
-    <link rel="icon" type="image/png" href="../resources/icon.png">
-    <link href="../styles/style.css" rel="stylesheet" type="text/css">
-    <style>
+<?php
 
-    </style>
-</head>
-<body>
-<table id="m-table" border="1">
+// x , y , r , shot or miss , start , time
 
-    <tr>
-        <td>
-            <header>
-                Rogachev Kirill Olegovich P3230 2621
-            </header>
-        </td>
-    </tr>
+function make_shot($X, $Y, $R)
+{
+    $result = $X . ' ' . $Y . ' ' . $R . ' ';
+    if ($X > 0) {
+        if ($Y > 0) {
+            $result .= 'NO' . ' ';
+        } else {
+            if ($X ^ 2 + $Y ^ 2 <= $R ^ 2) $result .= 'YES' . ' ';
+            else $result .= 'NO' . ' ';
+        }
+    } else {
+        if ($Y > 0) {
+            if ($X * (-1) + $Y * 2 <= $R) $result .= 'YES' . ' ';
+            else $result .= 'NO' . ' ';
+        } else {
+            if ($X * (-1) <= ($R / 2) and $Y * (-1) <= $R) $result .= 'YES' . ' ';
+            else $result .= 'NO' . ' ';
+        }
+    }
 
-    <tr>
-        <td colspan="10">
-            <div>
-                <?php
+    return $result;
+}
 
-                // x , y , r , shot or miss , start , time
+function write_shot($shot)
+{
+    $shot_elements = str_getcsv($shot, ' ');
+    echo '<tr><td class="x-class">' . $shot_elements[0] . '</td>';
+    echo '<td class="y-class">' . $shot_elements[1] . '</td>';
+    echo '<td class="r-class">' . $shot_elements[2] . '</td>';
+    echo '<td class="som-class">' . $shot_elements[3] . '</td>';
+    echo '<td class="stt-class">' . $shot_elements[4] . '</td>';
+    echo '<td class="sct-class">' . $shot_elements[5] . '</td></tr>';
+}
 
-                function make_shot($X, $Y, $R)
-                {
-                    $result = $X . ' ' . $Y . ' ' . $R . ' ';
-                    if ($X > 0) {
-                        if ($Y > 0) {
-                            $result .= 'NO' . ' ';
-                        } else {
-                            if ($X ^ 2 + $Y ^ 2 <= $R ^ 2) $result .= 'YES' . ' ';
-                            else $result .= 'NO' . ' ';
-                        }
-                    } else {
-                        if ($Y > 0) {
-                            if ($X * (-1) + $Y * 2 <= $R) $result .= 'YES' . ' ';
-                            else $result .= 'NO' . ' ';
-                        } else {
-                            if ($X * (-1) <= ($R / 2) and $Y * (-1) <= $R) $result .= 'YES' . ' ';
-                            else $result .= 'NO' . ' ';
-                        }
-                    }
+function writePrevTable()
+{
+    echo '<table id="page-table-T" border="1">';
+    echo '<tr><th id="x-id">' . "X" . '</th>';
+    echo '<th class="y-class">' . "Y" . '</th>';
+    echo '<th class="r-class">' . "R" . '</th>';
+    echo '<th class="som-class">' . "Y/N" . '</th>';
+    echo '<th class="stt-class">' . "Start time" . '</th>';
+    echo '<th class="sct-class">' . "Script time" . '</th>';
+    echo '<th width="20px"></th></tr>';
+    echo '<tr><td colspan="7"><div id="scroll-container"><table border="1">';
+    $prev_shots = file('shotTable.csv');
+    foreach ($prev_shots as $shot) {
+        write_shot($shot);
+    }
+}
 
-                    return $result;
-                }
+$XGet = (int)htmlspecialchars($_GET['answerX']);
+$YGet = (double)htmlspecialchars($_GET['answerY']);
+$RGet = (float)htmlspecialchars($_GET['answerR']);
 
-                function write_shot($shot)
-                {
-                    $shot_elements = str_getcsv($shot, ' ');
-                    echo '<tr><td class="x-class">' . $shot_elements[0] . '</td>';
-                    echo '<td class="y-class">' . $shot_elements[1] . '</td>';
-                    echo '<td class="r-class">' . $shot_elements[2] . '</td>';
-                    echo '<td class="som-class">' . $shot_elements[3] . '</td>';
-                    echo '<td class="stt-class">' . $shot_elements[4] . '</td>';
-                    echo '<td class="sct-class">' . $shot_elements[5] . '</td></tr>';
-                }
+$start = date("d.m.Y;H:i:s:u");
+$time = microtime(true);
+writePrevTable();
 
-                function writePrevTable()
-                {
-                    echo '<table id="page-table" border="1">';
-                    echo '<tr><th id="x-id">' . "X" . '</th>';
-                    echo '<th class="y-class">' . "Y" . '</th>';
-                    echo '<th class="r-class">' . "R" . '</th>';
-                    echo '<th class="som-class">' . "Y/N" . '</th>';
-                    echo '<th class="stt-class">' . "Start time" . '</th>';
-                    echo '<th class="sct-class">' . "Script time" . '</th>';
-                    echo '<th width="20px"></th></tr>';
-                    echo '<tr><td colspan="7"><div id="scroll-container"><table border="1">';
-                    $prev_shots = file('shotTable.csv');
-                    foreach ($prev_shots as $shot) {
-                        write_shot($shot);
-                    }
-                }
-
-                $XGet = htmlspecialchars($_GET['answerX']);
-                $YGet = htmlspecialchars($_GET['answerY']);
-                $RGet = htmlspecialchars($_GET['answerR']);
-
-                $start = date("d.m.Y;H:i:s:u");
-                $time = microtime(true);
-                writePrevTable();
-
-                if (isset($_GET['answerX']) and isset($_GET['answerY']) and isset($_GET['answerR'])) {
-                    if ($XGet >= -4 and $XGet <= 4 and $YGet >= -5 and $YGet <= 3 and $RGet >= 1 and $RGet <= 3) {
-                        $fp = fopen("shotTable.csv", "a");
-                        $shot = make_shot($XGet, $YGet, $RGet);
-                        $time = microtime(true) - $time;
-                        $shot .= $start . ' ' . number_format($time, 10);
-                        write_shot($shot);
-                        $check = fwrite($fp, $shot . "\r\n");
-                        fclose($fp);
-                        echo '</table></div></td></tr>';
-                        if (!$check) echo '<tr><td colspan="6"><div class="warning">' . "Error not write" . '</div></td></tr>';
-                        echo '</table>';
-                    } else {
-                        echo '</table></div></td></tr>';
-                        echo '<tr><td colspan="6"><div class="warning">' . "Wrong vars response: X=" . $XGet . " Y=" . $YGet . " R=" . $RGet . '</div></td></tr>';
-                        echo '</table>';
-                    }
-                } else {
-                    echo '</table></div></td></tr>';
-                    echo '<tr><td colspan="6"><div class="warning">' . "Variables doesn't set: X=" . $XGet . " Y=" . $YGet . " R=" . $RGet . '</div></td></tr>';
-                    echo '</table>';
-                }
-                ?>
-            </div>
-        </td>
-    </tr>
-
-</table>
-</body>
-</HTML>
+if (isset($_GET['answerX']) and isset($_GET['answerY']) and isset($_GET['answerR'])) {
+    if ($XGet >= -4 and $XGet <= 4 and $YGet >= -5 and $YGet <= 3 and $RGet >= 1 and $RGet <= 3) {
+        $fp = fopen("shotTable.csv", "a");
+        $shot = make_shot($XGet, $YGet, $RGet);
+        $time = microtime(true) - $time;
+        $shot .= $start . ' ' . number_format($time, 10);
+        write_shot($shot);
+        $check = fwrite($fp, $shot . "\r\n");
+        fclose($fp);
+        echo '</table></div></td></tr>';
+        if (!$check) echo '<tr><td colspan="6"><div class="warning">' . "Error not write" . '</div></td></tr>';
+        echo '</table>';
+    } else {
+        echo '</table></div></td></tr>';
+        echo '<tr><td colspan="6"><div class="warning">' . "Wrong vars response: X=" . $XGet . " Y=" . $YGet . " R=" . $RGet . '</div></td></tr>';
+        echo '</table>';
+    }
+} else {
+    echo '</table></div></td></tr>';
+    echo '<tr><td colspan="6"><div class="warning">' . "Variables doesn't set: X=" . $XGet . " Y=" . $YGet . " R=" . $RGet . ' '. $_GET . $_POST. '</div></td></tr>';
+    echo '</table>';
+}
+?>
